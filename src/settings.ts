9c97ce05d6f74ac6a682d0433f00931ce6f07193
Maciej Kulesza <mck.kulesza@gmail.com>
2020-10-10 23:08:27 +0200

    cleaning

--- src/settings.ts
+++
@@ -1,8 +0,0 @@
-const settings = {
-  database: {
-    uri: "mongodb://localhost:27017/?readPreference=primary&appname=testing&ssl=false",
-    dbName: "workScraper",
-  },
-};
-
-export default settings;
